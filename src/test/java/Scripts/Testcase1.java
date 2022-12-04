8e84f5a8cc17085e353d2a553e23515410d7b9fe
sbm7 <sbm7@US-7TG4XM3>
2022-12-04 12:01:55 -0800

    jenkins

---
+++ src/test/java/Scripts/Testcase1.java
@@ -0,0 +1,15 @@
+package Scripts;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase1 {
+	
+	@Test
+	public void tc1()
+	{
+		Reporter.log("Hello Java",true);
+	}
+	
+	
+
+}
